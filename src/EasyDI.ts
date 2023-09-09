417fef6c8d27e92c58d80e5dac643c6c2f2de7c1
dieppa <hm.dieppa@gmail.com>
2023-09-09 09:47:23 +0200

    Beacons are now global

--- src/EasyDI.ts
+++ src/EasyDI.ts
@@ -2,18 +2,18 @@ import Beacon from "./Beacon";
 import SingleBeacon from "./SingleBeacon";
 import BeaconNotFoundError from "./BeaconNotFoundError";
 
-const beacons = new Map<String, Beacon>()
+globalThis.beacons = new Map<String, Beacon>()
 
 function register<T>(
     name: String,
     creator: () => T,
     singleton: Boolean = false,
 ) {
-    beacons.set(name, buildBeacon(creator, singleton))
+    globalThis.beacons.set(name, buildBeacon(creator, singleton))
 }
 
 function inject<T>(name: String): T {
-    const beacon = beacons.get(name)
+    const beacon = globalThis.beacons.get(name)
     if (beacon == null) throw new BeaconNotFoundError(name)
     return beacon.create() as T
 }
